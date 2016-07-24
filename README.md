b4b5d1caf410616dedad89496f70d09b409e83ae
Josh <critesjosh@gmail.com>
2016-07-23 21:43:27 -0600

    added README, buttons work
    Added a README file.
    
    The buttons to hide and show subsets of users work.

---
+++ README.md
@@ -0,0 +1,6 @@
+#Twitch.tv Viewer
+
+See who is online and what they are streaming using this simple single page application.
+
+##How to use it.
+Visit this [link](#) to use the viewer.
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var currentlyStreaming = false;
 var streamURL = '';
-var regularStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var regularStreamers = ["fakeName", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 var ProfilePic;
 var displayName;
 
@@ -12,27 +12,37 @@ var getUserInfo = function(){
       console.log(data);
       displayName = data.display_name;
       profilePic = data.logo;
-      $('.container-fluid').append('<div class="row"><div class="info ' + user + ' col-sm-8 col-sm-offset-2"><img src=' + profilePic + '><p>' + displayName + '</p></div></div>');
-
-      $.ajax( "https://api.twitch.tv/kraken/streams/" + user, {
-        dataType: 'jsonp'
-      }).done(function(data){
-        console.log(data);
-        if (data.stream) {
-          streamURL = data.stream.channel.url;
-          var channel = data.stream.game;
-          $('.' + user).append('<p col-sm-2 >Online</p>');
-          $('.' + user).append('<a href=' + streamURL + '>Watch '+ channel + '</a>');
-        } else {
-          $('.' + user).toggleClass('offline');
-          $('.' + user).append('<p>Offline</p>');
-        }
-      });
-
+      if (data.error) {
+        $('.container-fluid').append('<div class="row"><div class="offline info ' + user + ' col-sm-8 col-sm-offset-2"><img src=' + profilePic + '><p>' + user + '</p></div></div>');
+        $('.' + user).append('<p>A user with that name does not exist.</p>');
+      } else {
+        $('.container-fluid').append('<div class="row"><div class="info ' + user + ' col-sm-8 col-sm-offset-2"><img src=' + profilePic + '><p>' + displayName + '</p></div></div>');
+        $.ajax( "https://api.twitch.tv/kraken/streams/" + user, {
+          dataType: 'jsonp'
+        }).done(function(data){
+          console.log(data);
+          if (data.stream) {
+            streamURL = data.stream.channel.url;
+            var channel = data.stream.game;
+            $('.' + user).append('<p col-sm-2 >Online</p>');
+            $('.' + user).append('<a href=' + streamURL + '>Watch '+ channel + '</a>');
+          } else {
+            $('.' + user).toggleClass('offline');
+            $('.' + user).append('<p>Offline</p>');
+          }
+        });
+      }
     });
+
   });
 };
 
 $(document).ready(function(){
   getUserInfo();
+  $('#online').click(function(){
+    $('.offline').hide();
+  });
+  $('#allUsers').click(function(){
+    $('.offline').show();
+  });
 });
